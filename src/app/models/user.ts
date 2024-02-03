38d30360747c88f7983e2708b93443ee10246bf5
ANIKET  WANI <101266471+ABW1729@users.noreply.github.com>
2024-02-03 14:51:43 +0530

    Update user.ts

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,14 +1,1 @@
-import mongoose,{Schema,models} from "mongoose";
 
-
-const userSchema = new mongoose.Schema({
-   name:{type:String,required:true},
-   points:{type:Number,required:true},
-   email:{type:String,required:true},
-   phoneNumber:{type:String,required:true}
-  });
-  
-
-  const User = models.User || mongoose.model("User", userSchema);
-  
-  export default User;
